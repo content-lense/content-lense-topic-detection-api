ba3a9b8a5bbff00ae989d4dacb2389c8ac6a8203
Markus Hörmann <markus@focal-analytics.com>
2022-10-25 13:05:02 +0200

    added basic stats and wiener sachtextindex

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+import pipeline
+
+app = Flask(__name__)
+
+@app.route('/articles', methods=['POST'])
+def postArticles():
+    data = request.json
+    res=pipeline.processArticle(data)
+    return res
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')  # run our Flask app
---
+++ pipeline.py
@@ -0,0 +1,73 @@
+import textstat
+import spacy
+import textdescriptives as td
+
+# str = "de" | "en"
+def calc_statistics(txt: str, language: str = "en"):
+    textstat.set_lang(language)
+    wienerSachtextIndex = textstat.wiener_sachtextformel(txt, 1)
+    readingTimeInMin = textstat.reading_time(txt, ms_per_char=14.69)
+    totalSyllables = textstat.syllable_count(txt)
+    totalWords = textstat.lexicon_count(txt, removepunct=True)
+    totalSentences = textstat.sentence_count(txt)
+    totalChars = textstat.char_count(txt, ignore_spaces=True)
+    totalLetters = textstat.letter_count(txt, ignore_spaces=True)
+    totalWordsLongerThanThreeSyllables = textstat.polysyllabcount(txt)
+    totalSingleSyllableWords = textstat.monosyllabcount(txt)
+
+    nlp = spacy.load("de_dep_news_trf" if language == "de" else "en_core_web_trf")
+    nlp.add_pipe("textdescriptives")
+    doc = nlp(txt)
+    stats_dict_counts = doc._.counts
+    totalUniqueWords = stats_dict_counts["n_unique_tokens"]
+    stats_dict_sentence_length = doc._.sentence_length
+    meanWordsPerSentence = stats_dict_sentence_length["sentence_length_mean"]
+    medianWordsPerSentence = stats_dict_sentence_length["sentence_length_median"]
+    stats_dict_token_length = doc._.token_length
+    meanCharsPerWord = stats_dict_token_length["token_length_mean"]
+    medianCharsPerWord = stats_dict_token_length["token_length_median"]
+
+    res_dict = {
+        "scores": {
+            "wienerSachtextIndex": wienerSachtextIndex,
+            "readingTimeInMinutes": readingTimeInMin
+        },
+        "descriptives": {
+            "totalSyllables": totalSyllables,
+            "totalChars": totalChars,
+            "totalLetters": totalLetters,
+            "totalWords": totalWords,
+            "totalSentences": totalSentences,
+            "meanWordsPerSentence": meanWordsPerSentence,
+            "meanCharsPerWord": meanCharsPerWord,
+            "medianWordsPerSentence": medianWordsPerSentence,
+            "medianCharsPerWord": medianCharsPerWord,
+            "totalWordsLongerThanThreeSyllables": totalWordsLongerThanThreeSyllables,
+            "totalSingleSyllableWords": totalSingleSyllableWords,
+            "totalUniqueWords": totalUniqueWords,
+            "averageWordsPerSentence": totalWords/totalSentences
+        }
+    }
+    return res_dict
+
+
+def processArticle(article):
+    print("Process Article")
+    res = {
+        "heading": {},
+        "summary": {},
+        "body": {}
+    }
+
+    try:
+        for part in article:
+            if (part in ["heading", "summary", "body"] & type(article[part]) == str ):
+                print("calculate %s" % (part))
+                part_json = calc_statistics(article[part])
+                res[part] = part_json
+        print("Finished: ", res)
+
+    except Exception as e:
+        print("error", e)
+
+    return res
